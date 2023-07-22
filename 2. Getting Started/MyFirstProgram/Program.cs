730dac3579304007880620908db3cf95ad65a766
SumitK27 <sumitkajbaje@gmail.com>
2023-07-22 22:59:44 +0530

    C# Getting Started

---
+++ 2. Getting Started/MyFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            MyUtilities.WeatherUtilities.Report("India", 27, 70);
+        }
+    }
+}
---
+++ 2. Getting Started/MyFirstProgram/WeatherUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyUtilities
+{
+    class WeatherUtilities
+    {
+        static float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
+        }
+
+        static float CelsiusToFahrenheit(float temperatureCelsius)
+        {
+            return (float)(temperatureCelsius * 1.8f + 32);
+        }
+
+        static float ComfortIndex(float temperatureFahrenheit, float humidityPercent) => (float)((temperatureFahrenheit + humidityPercent) / 4);
+
+        public static void Report(string location, float temperatureCelsius, float humidity)
+        {
+            var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
+            Console.WriteLine($"Comfort Index of: {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
+        }
+    }
+}
